29e8764aef6ecb01c31fa5fac419c824716246a8
Sindu Andita <sinduandita@gmail.com>
2020-10-03 02:25:51 +0700

    Background Generator

---
+++ script.js
@@ -0,0 +1,19 @@
+let css = document.querySelector("h3");
+let inputColor1 = document.querySelector(".color1");
+let inputColor2 = document.querySelector(".color2");
+let body = document.getElementById("gradient");
+
+inputColor1.addEventListener("input", setGradient);
+
+inputColor2.addEventListener("input", setGradient);
+
+function setGradient() {
+  body.style.background =
+    "linear-gradient(to right, " +
+    inputColor1.value +
+    "," +
+    " " +
+    inputColor2.value +
+    ")";
+  css.textContent = body.style.background;
+}
