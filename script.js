d1eb43fac8c310958ed7164a45a5590b883cd27b
Sindu Andita <sinduandita@gmail.com>
2020-10-09 00:09:08 +0700

    Final refactoring

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-let css = document.querySelector("h3");
-let inputColor1 = document.querySelector(".color1");
-let inputColor2 = document.querySelector(".color2");
-let body = document.getElementById("gradient");
+const css = document.querySelector("h3");
+const inputColor1 = document.querySelector(".color1");
+const inputColor2 = document.querySelector(".color2");
+const body = document.getElementById("gradient");
+
+const setGradient = () => {
+  body.style.background = `linear-gradient(to right, ${inputColor2.value}, ${inputColor1.value})`;
+  css.textContent = body.style.background + ";";
+};
 
 inputColor1.addEventListener("input", setGradient);
 
 inputColor2.addEventListener("input", setGradient);
-
-function setGradient() {
-  body.style.background =
-    "linear-gradient(to right, " +
-    inputColor1.value +
-    "," +
-    " " +
-    inputColor2.value +
-    ")";
-  css.textContent = body.style.background + ";";
-}
